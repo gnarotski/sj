b61dbe460e07ecd426bf5574bba47b94717fc49e
gnarotski <gnarotski@gmail.com>
2024-02-08 14:13:19 -0500

    Recent mods
    yea

--- Project1/SpaceJam.py
+++ Project1/SpaceJam.py
@@ -4,22 +4,28 @@ from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
+from shapeGenerator import Sphere #https://discourse.panda3d.org/t/generating-primitives/6630/8
 
 class SpaceJam(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
 		self.SetupScene()
 	def SetupScene(self):
-		self.Universe=self.loader.loadModel("./Assets/Universe/Universe.x")
-		self.Universe.reparentTo(self.render) #all grey
-		self.Universe.setScale(15000) #all white
-		self.Universe.setTexture(loader.loadTexture("./Assets/Universe/Universe-fors8M3.png"))
-		
-		self.Planet1=self.loader.loadModel("./Assets/Planets/protoPlanet.x")
-		self.Planet1.reparentTo(self.render)
-		self.Planet1.setPos(150,5000,67)
-		self.Universe.setScale(350)
-		#self.Universe.setTexture(loader.loadTexture("./Assets/Planets/protoPlanet.png"))
-		
+		self.SetupCam()
+		self.Universe=Celestial("./Assets/Universe/Universe.x",0,0,0,15000,"./Assets/Universe/Universe-fors8M3.png",self)
+		self.Planet1=Celestial(Sphere(1),150,5000,67,100,"./Assets/Planets/textures/2k_neptune.jpg",self)
+		self.Sun=Celestial(Sphere(1),0,0,0,1000,"./Assets/Extra/2k_sun.jpg",self)
+	def SetupCam(self):
+		self.trackball.node().set_pos(0,10000,0)
+class Celestial:
+	def __init__(self,modelPath,x,y,z,scale,texturePath,main):
+		if type(modelPath) is str:
+			self.model=main.loader.loadModel(modelPath)
+		else:
+			self.model=modelPath
+		self.model.reparentTo(main.render)
+		self.model.setPos(x,y,z)
+		self.model.setScale(scale)
+		self.model.setTexture(loader.loadTexture(texturePath))
 app = SpaceJam()
 app.run()
