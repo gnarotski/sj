ae110a9ee1daeb87d43c762adf106f2c9d6d6c01
gnarotski <gnarotski@gmail.com>
2024-02-14 22:31:27 -0500

    Added more
    Added the needed planets, player spaceship, and space station.

--- Project1/SpaceJam.py
+++ Project1/SpaceJam.py
@@ -4,7 +4,6 @@ from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
-from shapeGenerator import Sphere #https://discourse.panda3d.org/t/generating-primitives/6630/8
 
 class SpaceJam(ShowBase):
 	def __init__(self):
@@ -13,8 +12,16 @@ class SpaceJam(ShowBase):
 	def SetupScene(self):
 		self.SetupCam()
 		self.Universe=Celestial("./Assets/Universe/Universe.x",0,0,0,15000,"./Assets/Universe/Universe-fors8M3.png",self)
-		self.Planet1=Celestial(Sphere(1),150,5000,67,100,"./Assets/Planets/textures/2k_neptune.jpg",self)
-		self.Sun=Celestial(Sphere(1),0,0,0,1000,"./Assets/Extra/2k_sun.jpg",self)
+		self.Planet1=Celestial("./Assets/Planets/protoPlanet.x",1000,5000,0,200,"./Assets/Planets/textures/2k_neptune.jpg",self)
+		self.Planet1=Celestial("./Assets/Planets/protoPlanet.x",700,5500,200,300,"./Assets/Planets/textures/2k_jupiter.jpg",self)
+		self.Planet1=Celestial("./Assets/Planets/protoPlanet.x",500,6000,300,30,"./Assets/Planets/textures/2k_mars.jpg",self)
+		self.Planet1=Celestial("./Assets/Planets/protoPlanet.x",400,6500,600,40,"./Assets/Planets/textures/2k_mercury.jpg",self)
+		self.Planet1=Celestial("./Assets/Planets/protoPlanet.x",200,7000,800,110,"./Assets/Planets/textures/2k_saturn.jpg",self)
+		self.Planet1=Celestial("./Assets/Planets/protoPlanet.x",150,7500,900,106,"./Assets/Planets/textures/2k_uranus.jpg",self)
+		self.Sun=Celestial("./Assets/Planets/protoPlanet.x",0,0,0,1000,"./Assets/Extra/2k_sun.jpg",self)
+		self.PlayerSpaceship=Celestial("./Assets/Spaceships/theBorg/theBorg.x",0,0,5000,100,"./Assets/Spaceships/theBorg/theBorg.jpg",self)
+		self.Spacestation=Celestial("./Assets/Space Station/SpaceStation1B/spaceStation.x",5000,0,0,100,"./Assets/Space Station/SpaceStation1B/SpaceStation1_Dif2.png",self)
+		
 	def SetupCam(self):
 		self.trackball.node().set_pos(0,10000,0)
 class Celestial:
